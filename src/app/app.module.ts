623113b21296b52bfb66b0cbbf057c8085f14f35
Kyle Collie <kcollie@hotmail.com>
2018-02-28 23:13:37 -0500

    working list item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
-import { MatButtonModule, MatToolbarModule } from "@angular/material";
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatListModule
+} from "@angular/material";
 
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
-import { HomeComponent } from './home/home.component';
-import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+import { HomeComponent } from "./home/home.component";
+import { PathNotFoundComponent } from "./path-not-found/path-not-found.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PathNotFoundComponent],
@@ -17,7 +21,9 @@ import { PathNotFoundComponent } from './path-not-found/path-not-found.component
     BrowserAnimationsModule,
     AppRoutingModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
