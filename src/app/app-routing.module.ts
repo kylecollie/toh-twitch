660d61eb1b79627196dae85ff819b411ac405df2
Kyle Collie <kcollie@hotmail.com>
2018-03-03 10:38:46 -0500

    fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+
 import { HomeComponent } from "./home/home.component";
 import { PathNotFoundComponent } from "./path-not-found/path-not-found.component";
 
